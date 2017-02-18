f9bdbb2250a72743f91d40381b97ddcfb426dbef
Arthur De Araujo <wafflez180@yahoo.com>
2017-02-18 03:17:06 -0500

    Added nav bar and logout to ui. Added custom fb login button with Radar, FBSDKs and local notifcation integration

--- boyz/boyz/AppDelegate.swift
+++ boyz/boyz/AppDelegate.swift
@@ -7,40 +7,97 @@
 //
 
 import UIKit
+import RadarSDK
+import FacebookCore
+import FacebookLogin
+import FacebookShare
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FBSDKShareKit
+import NotificationCenter
+import UserNotifications
+import UserNotificationsUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Radar.initialize(publishableKey: "org_live_pk_dcc33d696849695d6ad8376ba0669eba6b7c45f5");
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        // iOS 10 support
+        if #available(iOS 10, *) {
+            UNUserNotificationCenter.current().requestAuthorization(options:[.badge, .alert, .sound]){ (granted, error) in }
+            application.registerForRemoteNotifications()
+        }
+            // iOS 9 support
+        else if #available(iOS 9, *) {
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+            // iOS 8 support
+        else if #available(iOS 8, *) {
+            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+            // iOS 7 support
+        else {
+            application.registerForRemoteNotifications(matching: [.badge, .sound, .alert])
+        }
+        
         return true
     }
-
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        if let _ = Bundle.main.object(forInfoDictionaryKey: "FacebookAppID") as? String {
+            return SDKApplicationDelegate.shared.application(application,
+                                                             open: url,
+                                                             sourceApplication: sourceApplication,
+                                                             annotation: annotation)
+        }
+        
+        return false
+    }
+    
+    @available(iOS 9.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        
+        if let _ = Bundle.main.object(forInfoDictionaryKey: "FacebookAppID") as? String {
+            return SDKApplicationDelegate.shared.application(application, open: url, options: options)
+        }
+        
+        return false
+    }
+    
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
--- boyz/boyz/ViewController.swift
+++ boyz/boyz/ViewController.swift
@@ -7,19 +7,146 @@
 //
 
 import UIKit
+import RadarSDK
+import CoreLocation
+import FacebookLogin
+import FacebookCore
+import FacebookShare
+import FBSDKLoginKit
+import FBSDKCoreKit
+import FBSDKShareKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RadarDelegate {
+    
+    let myLoginButton = UIButton(type: .custom)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let accessToken = AccessToken.current {
+            print("Facebook Access Token: ", accessToken.userId!);
+            getFacebookUserData()
+        }else if myLoginButton.superview == nil {
+            configureFBLoginButton()
+        }
+    }
+    
+    func configureFBLoginButton(){
+        myLoginButton.backgroundColor = UIColor.darkGray
+        
+        let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 180, height: 40))
+        myLoginButton.frame = rect;
+        myLoginButton.center = view.center;
+        myLoginButton.setTitle("My Login Button", for: .normal)
+        
+        // Handle clicks on the button
+        myLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
+        
+        // Add the button to the view
+        view.addSubview(myLoginButton)
+    }
 
+    // Once the button is clicked, show the login dialog
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile, .userFriends, .email ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("Logged in!")
+                self.getFacebookUserData()
+            }
+        }
+    }
+    
+    func getFacebookUserData(){
+        print("Getting facebook data")
+        let connection = GraphRequestConnection()
+        let params = ["fields" : "email, name"]
+        
+        connection.add(GraphRequest(graphPath: "/me", parameters: params)) { httpResponse, result in
+            switch result {
+            case .success(let response):
+                if let responseDictionary = response.dictionaryValue {
+                    print("Graph Request Succeeded: \(responseDictionary)")
+                    
+                    self.configureRadar(userId: responseDictionary["id"] as! String, email: responseDictionary["email"] as! String, fullName: responseDictionary["name"] as! String)
+                }
+            case .failed(let error):
+                print("Graph Request Failed: \(error)")
+            }
+        }
+        connection.start()
+    }
+    
+    func configureRadar(userId:String, email:String, fullName:String){
+        // User is logged in, use 'accessToken' here.
+        let status = Radar.authorizationStatus()
+        
+        print("Radar Authorized Status: ", status);
+        print("Radar Started Tracking");
+        
+        Radar.requestAlwaysAuthorization()
+        
+        Radar.setUserId(userId)
+        
+        Radar.setDescription(fullName);
+        //Radar.setDescription(fullName + " : " + email);
+        Radar.startTracking()
+        Radar.setDelegate(self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func didReceiveEvents(_ events: [RadarEvent], user: RadarUser) {
+        print("Received Event!")
+        print(events)
+        // do something with events, user
+        let center = UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [.alert, .sound];
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Boyz"
+        content.body = "Spagettttt"
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(
+            timeInterval: 1.0,
+            repeats: false)
+        
+        let request = UNNotificationRequest(
+            identifier: "1.second.message",
+            content: content,
+            trigger: trigger
+        )
+        
+        UNUserNotificationCenter.current().add(
+            request, withCompletionHandler: nil)
+    }
+    
+    func didFail(status: RadarStatus) {
+        print("Failed Event!")
+        
+        // do something with status
+    }
+    
+    //MARK: UBActions
+    
+    @IBAction func pressedLogout(_ sender: Any) {
+        //Logout of facebook
+    }
+    
+    
+    
 }
 
